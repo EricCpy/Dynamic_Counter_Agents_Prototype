60d1588393ede38b78668d45a3be45b2ddaa78e7
Eric <heimleiter.dasheim@gmail.com>
2024-11-27 23:23:16 +0100

    remove random featured items

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -7,21 +7,5 @@ import {items} from '../items';
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-  currentIndex: number = 0;
-  products = items;
-
-  ngOnInit(): void {
-    let selectedProducts = [];
-    let randomIndexes: number[] = [];
-
-    while (selectedProducts.length < 3) {
-      let randomIndex = Math.floor(Math.random() * this.products.length);
-      if (!randomIndexes.includes(randomIndex)) {
-        selectedProducts.push(this.products[randomIndex]);
-        randomIndexes.push(randomIndex);
-      }
-    }
-
-    this.products = selectedProducts;
-  }
+  products = items.slice(4, 7);
 }
